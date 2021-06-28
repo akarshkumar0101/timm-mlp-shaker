9993d874c973834aaa3a07ce5d4501e379b15c5c
Akarsh Kumar <akarshkumar0101@gmail.com>
2021-06-28 18:41:19 -0500

    external implementation of mlp-mixer

--- mlp_mixer_pytorch.py
+++ mlp_mixer_pytorch.py
@@ -37,3 +37,19 @@ def MLPMixer(*, image_size, channels, patch_size, dim, depth, num_classes, expan
         nn.Linear(dim, num_classes),
         nn.LogSoftmax(dim=-1)
     )
+
+def MLPMixerNoHead(*, image_size, channels, patch_size, dim, depth, num_classes, expansion_factor = 4, dropout = 0.):
+    assert (image_size % patch_size) == 0, 'image must be divisible by patch size'
+    num_patches = (image_size // patch_size) ** 2
+    chan_first, chan_last = partial(nn.Conv1d, kernel_size = 1), nn.Linear
+
+    return nn.Sequential(
+        Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+        nn.Linear((patch_size ** 2) * channels, dim),
+        *[nn.Sequential(
+            PreNormResidual(dim, FeedForward(num_patches, expansion_factor, dropout, chan_first)),
+            PreNormResidual(dim, FeedForward(dim, expansion_factor, dropout, chan_last))
+        ) for _ in range(depth)],
+        nn.LayerNorm(dim),
+        Reduce('b n c -> b c', 'mean'),
+    )
