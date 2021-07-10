40381f265366a939466d32ec7b657101fc266e5d
Akarsh Kumar <akarshkumar0101@gmail.com>
2021-07-09 20:23:48 -0500

    improve dim layernorm and dim linear models

--- dim_models.py
+++ dim_models.py
@@ -1,9 +1,12 @@
+import numpy as np
 import torch
 from torch import nn
 
 import copy
 
-class DimLinear(nn.Linear):
+import util
+
+class DimLinearManual(nn.Linear):
     """
     This module is simply a linear layer that is applied to an arbritrary dimension rather than the last dimension.
     """
@@ -52,17 +55,20 @@ class DimLinear(nn.Conv1d):
         self.input_shape = list(shape)
         self.output_shape = copy.copy(self.input_shape)
         self.output_shape[dim_to_mix] = out_features
-        self.dim_to_mix = dim_to_mix 
+        
+        
+        self.ndims = len(self.input_shape)
+        self.dim_from_left = dim_to_mix if dim_to_mix>=0 else dim_to_mix+self.ndims
+        self.dim_from_right = self.dim_from_left-self.ndims
         
     def forward(self, x, verbose=False):
         bs, is_ = util.bs_is_split(x.shape, np.prod(self.input_shape, dtype=int))
-        if list(is_) != self.input_shape:
-            raise Exception(f'Input shape {is_} does not match expected input shape {self.input_shape}')
+        assert list(is_) == self.input_shape, f'Input shape {is_} does not match expected input shape {self.input_shape}'
             
         bsis = [*bs, *self.input_shape]
         bsos = [*bs, *self.output_shape]
-        left_collapse = np.prod([*bs, *is_[:self.dim_to_mix]], dtype=int)
-        right_collapse = np.prod([*is_[self.dim_to_mix+1:]], dtype=int)
+        left_collapse = np.prod([*bs, *is_[:self.dim_from_left]], dtype=int)
+        right_collapse = np.prod([*is_[self.dim_from_left+1:]], dtype=int)
         proc_shape = [left_collapse, self.in_features, right_collapse]
         
         if verbose:
@@ -75,27 +81,27 @@ class DimLinear(nn.Conv1d):
     
 class DimLayerNorm(nn.LayerNorm):
     """
-    This module is simply a layer norm that is applied to an arbritrary dimension rather than the last dimension or all the dimensions.
+    This module is simply a layer norm that is applied to arbritrary dimensions rather than the last dimension or all the dimensions.
     """
-    def __init__(self, shape, dim_to_mix=None, eps=1e-5, elementwise_affine=True):
+    def __init__(self, shape, dims_to_mix=[-1], eps=1e-5, elementwise_affine=True):
+        if type(dims_to_mix) is int:
+            dims_to_mix = [dims_to_mix]
+            
         self.input_shape = list(shape)
-        self.normalized_shape = [1 for l in self.input_shape]
-        self.normalized_shape[dim_to_mix] = self.input_shape[dim_to_mix]
-        super().__init__(self.normalized_shape, elementwise_affine=elementwise_affine)
         
-        self.dim_to_mix = dim_to_mix 
+        self.ndims = len(self.input_shape)
+        self.dims_from_left = [(d if d>=0 else d+self.ndims) for d in dims_to_mix]
+        self.dims_from_right = [d-self.ndims for d in self.dims_from_left]
         self.eps = eps
         
-        self.n_dims_from_right = len(self.input_shape)-self.dim_to_mix-1
+        self.normalized_shape = [(l if i in self.dims_from_left else 1) for i, l in enumerate(self.input_shape)]
+        super().__init__(self.normalized_shape, elementwise_affine=elementwise_affine)
         
     def forward(self, x):
         assert list(x.shape[-len(self.input_shape):])==self.input_shape, 'input shape not correct'
-        dim = -self.n_dims_from_right-1
-        mean = x.mean(dim=dim, keepdim=True)
-#         self.eps = 1e-5
-        std = (x.var(dim=dim, keepdim=True, unbiased=False)+self.eps).sqrt()
+        dims = tuple(self.dims_from_right)
+        mean = x.mean(dim=dims, keepdim=True)
+        std = (x.var(dim=dims, keepdim=True, unbiased=False)+self.eps).sqrt()
         x = (x-mean)/std
-        return x*self.weight+self.bias
-    
-    
+        return torch.addcmul(self.bias, x, self.weight)    
     
